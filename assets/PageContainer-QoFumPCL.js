8f1bfaa12b8923560147c65bb20b203a281e64b9
Ariza25 <Ariza25@users.noreply.github.com>
2026-06-16 16:29:42 +0000

    deploy: Ariza25/menu-shopping@ecf1a13c5d33fdad83eab4b608f9fd74d5d8e47c

--- assets/PageContainer-QoFumPCL.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-DV-sc-u2.js";function s({children:e,className:o,spacingClassName:a="space-y-6"}){return n.jsx("div",{className:[a,o].filter(Boolean).join(" "),children:e})}export{s as P};
