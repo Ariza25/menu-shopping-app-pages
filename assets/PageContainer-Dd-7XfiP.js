88583fb82147a0bf017373f585472e5171510a35
Ariza25 <Ariza25@users.noreply.github.com>
2026-06-21 12:15:52 +0000

    deploy: Ariza25/menu-shopping@0ac930a02238964f487e73de005d79413dcfa274

---
+++ assets/PageContainer-Dd-7XfiP.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-AWC8_pOy.js";function s({children:e,className:o,spacingClassName:a="space-y-6"}){return n.jsx("div",{className:[a,o].filter(Boolean).join(" "),children:e})}export{s as P};
