4140135c8d0022cb4edda43a65c3b0e5382cadb8
Ariza25 <Ariza25@users.noreply.github.com>
2026-07-01 20:20:56 +0000

    deploy: Ariza25/menu-shopping@c60ce12476ef083a0a63b19b35774db0a2f4bd95

---
+++ assets/PageContainer-fyi65L_r.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-CVenz5bR.js";function s({children:e,className:o,spacingClassName:a="space-y-6"}){return n.jsx("div",{className:[a,o].filter(Boolean).join(" "),children:e})}export{s as P};
