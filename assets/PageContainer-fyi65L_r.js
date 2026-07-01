e80cd0fe0667195f92d9741067e88fe041fdcc48
Ariza25 <Ariza25@users.noreply.github.com>
2026-07-01 21:31:01 +0000

    deploy: Ariza25/menu-shopping@6ca2aed245047369a706cebb82c3ae964775473a

--- assets/PageContainer-fyi65L_r.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-CVenz5bR.js";function s({children:e,className:o,spacingClassName:a="space-y-6"}){return n.jsx("div",{className:[a,o].filter(Boolean).join(" "),children:e})}export{s as P};
